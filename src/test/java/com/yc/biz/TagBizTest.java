b3c5d7af9d4d1922459b940eef27e886f045864d
qqq <qqq@DESKTOP-BICVE90>
2019-09-19 16:38:53 +0800

    aa

--- src/test/java/com/yc/biz/TagBizTest.java
+++ src/test/java/com/yc/biz/TagBizTest.java
@@ -17,7 +17,7 @@ public class TagBizTest {
 	@Test
 	public void testFindByCondition() throws Exception {
 		System.out.println(biz.findByCondition(null));
-		System.out.println("1111111111111111111rgtgt1");
+		System.out.println("1111111111111hvf111111rgtgt1");
 		System.out.println("22222222222222222222");
 	}
 
